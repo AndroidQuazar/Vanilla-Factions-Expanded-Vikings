6ed5a97cbe98972f58e3abbd393c0c6f2ebd4915
Helixien <58004472+Helixien@users.noreply.github.com>
2020-07-07 21:08:38 +0200

    Added faction, fixed errors

---
+++ 1.1/Source/TNOR/TNOR/TNOR_DefOf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace TNOR
+{
+    [DefOf]
+    public static class TNOR_DefOf
+    {
+        public static ThingDef TNR_Apiary;
+        public static JobDef TNR_TakeHoneyOutOfApiary;
+        public static JobDef TNR_TendToApiary;
+        public static HediffDef TNR_Sting;
+        public static ThoughtDef TNR_StingMoodDebuff;
+        public static ThingDef TNR_Honey;
+        public static DamageDef TNR_DamageSting;
+    }
+}
