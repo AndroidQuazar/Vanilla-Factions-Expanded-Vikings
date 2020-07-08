8a4226e83b238b7c35d62f37a8a0ef3bd2e6cfca
Taranchuk <nfhfyxer@gmail.com>
2020-07-08 13:25:33 +0300

    update

---
+++ 1.1/Source/VFEV/CompProperties_PawnPsyWaver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace VFEV
+{
+	public class CompProperties_PawnPsyWaver : CompProperties
+	{
+		public CompProperties_PawnPsyWaver()
+		{
+			this.compClass = typeof(CompPawnPsyWaver);
+		}
+
+		public float maxDistance;
+
+		public IntRange interval;
+
+		public int stunPeriod;
+	}
+}
+
