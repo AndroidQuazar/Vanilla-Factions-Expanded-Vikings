1edd2d1a7d90be96b68a95d7d7e71e9c27c64cd3
Kylian <kylian.balde@gmail.com>
2020-07-20 15:17:49 +0200

    Added all 3 beast hunt

---
+++ 1.1/Source/VFEV/VFEV_DefOf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace VFEV
+{
+    [DefOf]
+    class VFEV_DefOf
+    {
+        #pragma warning disable 0649
+        public static PawnKindDef VFEV_Fenrir;
+        public static PawnKindDef VFEV_Lothurr;
+        public static PawnKindDef VFEV_Njorun;
+
+        public static ThoughtDef VFEI_BeastHunted;
+    }
+}
