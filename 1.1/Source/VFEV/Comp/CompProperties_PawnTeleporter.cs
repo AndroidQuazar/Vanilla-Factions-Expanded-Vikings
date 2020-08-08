30fc722ea08e52ec08c7205c6c9112180aceab95
Helixien <58004472+Helixien@users.noreply.github.com>
2020-08-08 15:29:09 +0200

    merged both builds

---
+++ 1.1/Source/VFEV/Comp/CompProperties_PawnTeleporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace VFEV
+{
+	public class CompProperties_PawnTeleporter : CompProperties
+	{
+		public CompProperties_PawnTeleporter()
+		{
+			this.compClass = typeof(CompPawnTeleporter);
+		}
+
+		public float minDistance;
+
+		public int cooldown;
+
+		public bool disableManhunterState = false;
+	}
+}
+
---
+++ 1.1/Source/VFEV/DeathActionWorker_MoodBoost.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace VFEV
+{
+    class DeathActionWorker_MoodBoost : DeathActionWorker
+    {
+        public override void PawnDied(Corpse corpse)
+        {
+            foreach (Map map in Find.Maps)
+            {
+                foreach (Pawn pawn in map.mapPawns.FreeColonists)
+                {
+                    pawn.needs.mood.thoughts.memories.TryGainMemory(VFEV_DefOf.VFEV_BeastHunted);
+                }
+            }
+        }
+    }
+}
