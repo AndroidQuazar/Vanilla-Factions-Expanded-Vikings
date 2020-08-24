7177bc774a3dc2357b417035d4651d7e6c009b11
Kylian <kylian.balde@gmail.com>
2020-08-24 16:26:17 +0200

    Added thrumbo hunt

--- 1.2/Source/VFEV/MapGeneration/BeastHuntEvents/GenStep_Thrumbo.cs
+++ 1.2/Source/VFEV/MapGeneration/BeastHuntEvents/GenStep_Thrumbo.cs
@@ -2,10 +2,12 @@
 using RimWorld.Planet;
 using Verse;
 using RimWorld;
+using Verse.AI.Group;
+using System.Collections.Generic;
 
 namespace VFEV.MapGeneration.BeastHuntEvents
 {
-    class GenStep_Lothurr : GenStep_Scatterer
+    class GenStep_Thrumbo : GenStep_Scatterer
     {
 		public override int SeedPart
 		{
@@ -22,8 +24,7 @@ namespace VFEV.MapGeneration.BeastHuntEvents
 
 		protected override void ScatterAt(IntVec3 loc, Map map, GenStepParams parms, int count = 1)
 		{
-			Pawn pawn = PawnGenerator.GeneratePawn(VFEV_DefOf.VFEV_Lothurr);
-			pawn.mindState.exitMapAfterTick = 120000;
+			Pawn pawn = PawnGenerator.GeneratePawn(PawnKindDefOf.Thrumbo);
 			GenSpawn.Spawn(pawn, loc, map, WipeMode.Vanish);
 			MapGenerator.rootsToUnfog.Add(loc);
 			MapGenerator.SetVar<CellRect>("RectOfInterest", CellRect.CenteredOn(loc, 1, 1));
