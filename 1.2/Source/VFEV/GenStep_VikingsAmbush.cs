58920b4c62c563fffd503cad5fe9c4d9ba48d920
Kylian <kylian.balde@gmail.com>
2020-08-28 14:13:24 +0200

    Finished legendary grave GenStep

--- 1.2/Source/VFEV/GenStep_VikingsAmbush.cs
+++ 1.2/Source/VFEV/GenStep_VikingsAmbush.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.AI.Group;
 
 namespace VFEV
 {
-    class GenStep_VikingsAmbush : GenStep_Scatterer
+    class GenStep_VikingsAmbush : GenStep
     {
 		public override int SeedPart
 		{
@@ -18,30 +19,27 @@ namespace VFEV
 			}
 		}
 
-		protected override bool CanScatterAt(IntVec3 c, Map map)
-		{
-			return base.CanScatterAt(c, map) && c.Standable(map);
-		}
-
-		protected override void ScatterAt(IntVec3 loc, Map map, GenStepParams parms, int count = 1)
+		public override void Generate(Map map, GenStepParams parms)
 		{
 			IncidentParms incidentParms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, map);
-			incidentParms.faction = Find.FactionManager.AllFactionsListForReading.Find(f=>!f.defeated && f.RelationKindWith(Faction.OfPlayer) == FactionRelationKind.Hostile && f.def.defName.Contains("VFEV_Vikings"));
-			incidentParms.points = StorytellerUtility.DefaultThreatPointsNow(incidentParms.target);
-
+			incidentParms.faction = Find.FactionManager.AllFactionsListForReading.Find(f => !f.defeated && f.RelationKindWith(Faction.OfPlayer) == FactionRelationKind.Hostile && f.def.defName.Contains("VFEV_Vikings"));
+			incidentParms.points = parms.sitePart.parms.threatPoints;
+			
 			List<Pawn> list = PawnGroupMakerUtility.GeneratePawns(IncidentParmsUtility.GetDefaultPawnGroupMakerParms(PawnGroupKindDefOf.Combat, incidentParms, false), true).ToList<Pawn>();
 			if (list.Count > 0)
-            {
-                foreach (Pawn pawn in list)
-                {
+			{
+				foreach (Pawn pawn in list)
+				{
 					IntVec3 result = new IntVec3();
 					CellFinder.TryFindRandomCellNear(map.Center, map, 10, c => c.Walkable(map), out result);
 					GenSpawn.Spawn(pawn, result, map);
-                }
-            }
+				}
+			}
 
-			MapGenerator.rootsToUnfog.Add(map.Center);
+			// MapGenerator.rootsToUnfog.Add(map.Center);
 			MapGenerator.SetVar<CellRect>("RectOfInterest", CellRect.CenteredOn(map.Center, 1, 1));
+
+			LordMaker.MakeNewLord(incidentParms.faction, new LordJob_DefendBase(incidentParms.faction, map.Center), map, list);
 		}
 	}
 }
