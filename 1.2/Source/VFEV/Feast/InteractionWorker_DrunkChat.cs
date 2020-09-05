e203f83cc694be786dc6dda8ed61724d5e4b2295
Taranchuk <nfhfyxer@gmail.com>
2020-09-05 17:16:24 +0300

    update

---
+++ 1.2/Source/VFEV/Feast/InteractionWorker_DrunkChat.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEV
+{
+	public class InteractionWorker_DrunkChat : InteractionWorker
+	{
+		public override float RandomSelectionWeight(Pawn initiator, Pawn recipient)
+		{
+            var alcoholHediff = initiator.health?.hediffSet?.GetFirstHediffOfDef(HediffDefOf.AlcoholHigh);
+            if (alcoholHediff != null && initiator.mindState?.duty?.def == VFEV_DefOf.VFEV_Feast)
+            {
+                if (alcoholHediff.CurStageIndex == 1) //tipsy
+                {
+                    return 1f;
+                }
+                if (alcoholHediff.CurStageIndex == 2) //drunk
+                {
+                    return 2f;
+                }
+            }
+            return 0f;
+        }
+	}
+}
