7e223aee6ef582b7d69888f41f8523e66cbe7f8f
Kylian <kylian.balde@gmail.com>
2020-09-16 14:46:36 +0200

    Apiary can no longer be builded under roof and need to be builded one tile away from any other apiary

---
+++ 1.2/Source/VFEV/Apiary/Placeworker_Apiary.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace VFEV
+{
+    class Placeworker_Apiary : PlaceWorker
+	{
+		public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			/*foreach (Thing apiary in map.listerBuildings.allBuildingsColonist.FindAll(b => b.def.defName == "VFEV_Apiary"))
+            {
+				if (apiary != null && apiary.CellsAdjacent8WayAndInside().Contains(center))
+                {
+					return "APlaceWorker".Translate();
+				}
+            }*/
+			CellRect cellAround = CellRect.CenteredOn(center, 1);
+            foreach (IntVec3 cell in cellAround)
+            {
+				if (cell.GetFirstBuilding(map)?.def?.defName == "VFEV_Apiary") return "APlaceWorker".Translate();
+			}
+			if (center.Roofed(map)) return "APlaceWorkerNoRoof".Translate();
+			return true;
+		}
+	}
+}
